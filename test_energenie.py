98093b2c190938951f542fb1f97a34457a06714c
Robert Franklin <rcf@mince.net>
2014-12-31 20:19:36 +0000

    test_energenie made v2 compatible and cleaned up

--- test_energenie.py
+++ test_energenie.py
@@ -1,42 +1,59 @@
 #!/usr/bin/env python3
 
+# test_energenie.py (2014-12-31) - test the Energenie library
+
+
+# if we're on Python 2.x, import the 3.x print() function
+
+from __future__ import print_function
+
+
 from energenie import *
 
-# We will just loop round switching the units on and off
+
+# set the input() function to use raw_input() so we can use the former on
+# Python 2.x; Python 3.x will fail because raw_input() does not exist, so we
+# just skip that
+
 try:
-    input('hit return key to send socket 1 ON code')
-    # Set K0-K3
-    print("sending code 1111 socket 1 on")
+    input = raw_input
+
+except NameError:
+    pass
+
+
+# test switch each of the sockets on and off
+
+try:
+    input("hit return key to send socket 1 ON code")
+    print("sending code %s socket 1 on" % bin(SOCK1_ON))
     send_code(SOCK1_ON)
 
-    input('hit return key to send socket 1 OFF code')
-    # Set K0-K3
-    print("sending code 0111 Socket 1 off")
+    input("hit return key to send socket 1 OFF code")
+    print("sending code %s socket 1 off" % bin(SOCK1_OFF))
     send_code(SOCK1_OFF)
 
-    input('hit return key to send socket 2 ON code')
-    # Set K0-K3
-    print("sending code 1110 socket 2 on")
+    input("hit return key to send socket 2 ON code")
+    print("sending code %s socket 2 on" % bin(SOCK2_ON))
     send_code(SOCK2_ON)
 
-    input('hit return key to send socket 2 OFF code')
-    # Set K0-K3
-    print("sending code 0110 socket 2 off")
+    input("hit return key to send socket 2 OFF code")
+    print("sending code %s socket 2 off" % bin(SOCK2_OFF))
     send_code(SOCK2_OFF)
 
-    input('hit return key to send ALL ON code')
-    # Set K0-K3
-    print("sending code 1011 ALL on")
+    input("hit return key to send ALL ON code")
+    print("sending code %s all on" % bin(ALL_ON))
     send_code(ALL_ON)
 
-    input('hit return key to send ALL OFF code')
-    # Set K0-K3
-    print("sending code 0011 All off")
+    input("hit return key to send ALL OFF code")
+    print("sending code %s all off" % bin(ALL_OFF))
     send_code(ALL_OFF)
 
 except KeyboardInterrupt:
+    # catch the interrupt signal and ignore it
     pass
 
-# Clean up the GPIOs for next time
-#except KeyboardInterrupt:
+
+# clean up the GPIOs
+
 cleanup()
